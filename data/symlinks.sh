dd543d1b1181f370f6bd1b31e3aad469d69b2a08
Stefan Wieczorek <stefan.wieczorek@weemedia.de>
2012-08-15 23:41:16 +0200

    add installation file

--- data/symlinks.sh
+++ data/symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # find and deletes dead symlinks
-#cd /var/www/magento1701/htdocs && find -L . -type l -delete
+#cd /var/www/magento1510/htdocs && find -L . -type l -delete
 
-lns -afFr /var/www/Mgt_CloudfrontInvalidation/htdocs/app/ /var/www/magento1701/htdocs/app/
+lns -afFr /var/www/Mgt_CloudfrontInvalidation/htdocs/app/ /var/www/magento1510/htdocs/app/
